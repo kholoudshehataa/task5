4eda964d76ac6ed83f71a4db7c8b945c7787c8ed
kholoudshehataa <116682727+kholoudshehataa@users.noreply.github.com>
2023-04-09 19:06:56 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,47 +1,4 @@
-// import 'dart:html';
 
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: MyHomePage(title: 'Birthday card'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key? key, required this.title}) : super(key: key);
-//   final String title;
-
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return new Container(
-//         color: Color.fromRGBO(11, 64, 66, 1),
-//         child: Row(
-//             children: [
-//           CircleAvatar(
-//             maxRadius: 50,
-//             backgroundImage: AssetImage('images/kho.jpg'),
-//           )
-//         ]));
-//   }
-// }
-/////////////////////////////////
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
